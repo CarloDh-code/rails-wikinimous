eae517b1bae54d2527b314c47426e5ff1031ffbb
Carlo <carlodhlln@gmail.com>
2024-11-12 16:31:03 +0100

    solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+require 'faker'
+
+
+
+
+
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title:    Faker::Book.title,
+    content: Faker::Book.author,
+  )
+  article.save!
+end
+puts 'Finished!'
